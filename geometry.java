83b0a7c6c1e5e3abd2e6963f1239eae593b09bce
Kennith_Joseph <sendmeamailatkennithjoseph@gmail.com>
2023-03-04 21:26:55 +0530

    Added area of rec

--- geometry.java
+++ geometry.java
@@ -2,4 +2,8 @@ public class geometry {
     public static int areaOfSquare(int a) {
         return a * a;
     }
+
+    public static int areaOfRectangle(int a, int b) {
+        return a * b;
+    }
 }
